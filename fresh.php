a34649afe8c87ec445eed2ff2a1445bc1c49dfcd
hitoy <vip@hitoy.org>
2018-03-12 18:30:16 +0800

    bug fixed

--- fresh.php
+++ fresh.php
@@ -13,7 +13,7 @@ $i=1;
 while(1){
     $tmp = $mysql->getRows("select id,linkname,category,post_time from posts order by id desc limit ".($i-1)*1000 .",1000");
     if(empty($tmp)) break;
-    $sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<urlset>\r\n";
+    $sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\" xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\r\n";
     foreach($tmp as $t){
         if($t['category']==''){
             $url= $host."/".str_replace("%post_id%",$t['id'],urlencode(str_replace("%post_name%",$t['linkname'],permanlink)));
